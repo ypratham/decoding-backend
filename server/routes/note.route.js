e19ae5ff602633672a02b76d3bb399336ff7a157
Pratham Yadav <ypratham0014@gmail.com>
2024-02-18 11:15:44 +0530

    chore: routes updated

--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -1,20 +1,23 @@
 import express from "express";
 import {
   createNote,
-  deleteNoteById,
-  fetchAllNotes,
-  fetchNoteById,
-  updateNoteById,
+  deleteNote,
+  fetchAllNodes,
+  updateNote,
 } from "../services/note.service.js";
 
 const router = express.Router();
 
-router.get("/", fetchAllNotes);
+//fetch all notes from db
+router.get("/", fetchAllNodes);
+
+//create note
 router.post("/create", createNote);
-router
-  .route("/:id")
-  .delete(deleteNoteById)
-  .put(updateNoteById)
-  .get(fetchNoteById);
+
+//update note
+router.put("/update/:id", updateNote);
+
+//delete note
+router.delete("/delete/:id", deleteNote);
 
 export { router as NoteRouter };
